67582e60280c557a09d00b5920a991a51e0ef737
Joshua Pereira <joshuathomaspereira@gmail.com>
2019-04-22 15:13:36 -0400

    added unit testing for airport model

---
+++ spec/models/airport_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Airport do
+  it { should have_valid(:name).when("Logan") }
+  it { should_not have_valid(:name).when(nil, "") }
+
+  it { should have_valid(:location).when("Boston") }
+  it { should_not have_valid(:location).when(nil, "") }
+
+  it { should have_valid(:airport_code).when("BOS") }
+  it { should_not have_valid(:airport_code).when(nil, "") }
+
+  it { should have_valid(:description).when("This is an airport!") }
+  it { should have_valid(:description).when(nil, "") }
+end
