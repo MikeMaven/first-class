efefb5deeb2cad407b260c4e381ecbb20c338a5b
Joshua Pereira <joshuathomaspereira@gmail.com>
2019-04-24 14:52:53 -0400

    Refactor code to use fetch-mock in enzyme test

--- spec/javascript/test/containers/AirportShowContainer.js
+++ spec/javascript/test/containers/AirportShowContainer.js
@@ -1,17 +1,28 @@
 import '../../testHelper.js'
+import fetchMock from 'fetch-mock'
 
 import AirportShowContainer from '../../../../app/javascript/react/containers/AirportShowContainer'
 import AirportShowTile from '../../../../app/javascript/react/components/AirportShowTile'
 
 describe('AirportShowContainer', () => {
-  let wrapper;
+  let airport,
+      wrapper;
 
   beforeEach(() => {
     jasmineEnzyme();
-    wrapper = shallow(
-      <AirportShowContainer />
+    airport = {
+      airport: {id: 1, name: "Logan", location: "Boston", airport_code: "BOS", description: "Anything"}
+    }
+    fetchMock.get('/api/v1/airports/1.json', {
+      status: 200,
+      body: airport
+    });
+    wrapper = mount(
+      <AirportShowContainer params={ { id: airport.airport.id } } />
     )
   })
+
+  afterEach(fetchMock.restore)
 
   it('should should have the specified initial state', () => {
     expect(wrapper.state()).toEqual({ airport: {id: '', name: '', airport_code: '', location: '', description: ''} });
@@ -22,13 +33,10 @@ describe('AirportShowContainer', () => {
     expect(wrapper.find('h1').text()).toContain("Airport")
   });
 
-  it('should render Airport Tile Components with specific props', () => {
-    wrapper.setState({ airport: {id: 1, name: "Logan", location: "Boston", airport_code: "BOS", description: "Anything"} });
-    expect(wrapper.find(AirportShowTile).props()).toEqual({
-      name: "Logan",
-      location: "Boston",
-      airport_code: "BOS",
-      description: "Anything"
-    });
+  it('should render Airport Tile Components with specific props', (done) => {
+    setTimeout(() => {
+      expect(wrapper.find(AirportShowTile).props()).toEqual({name: "Logan", location: "Boston", airport_code: "BOS", description: "Anything"})
+      done()
+    }, 0)
   });
 });
