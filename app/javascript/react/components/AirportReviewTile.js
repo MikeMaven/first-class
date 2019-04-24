98c6dc2d9bb5459ea734705e5d57ff8baa6a6540
Joshua Pereira <joshuathomaspereira@gmail.com>
2019-04-24 15:58:59 -0400

    finish review tiles

--- app/javascript/react/components/AirportReviewTile.js
+++ app/javascript/react/components/AirportReviewTile.js
@@ -5,6 +5,15 @@ const AirportReviewTile = props => {
     <div>
       <h3>{props.title}</h3>
       <p>{props.body}</p>
+      <ul>
+        <li>Overall Rating: {props.overall_rating}</li>
+        <li>Queue Time Rating: {props.queue_time}</li>
+        <li>Cleanliness Rating: {props.cleanliness}</li>
+        <li>Wifi Rating: {props.wifi}</li>
+        <li>Staff Rating: {props.staff}</li>
+        <li>Lounge Space Rating: {props.lounge_space}</li>
+      </ul>
+      <hr />
     </div>
   )
 }
