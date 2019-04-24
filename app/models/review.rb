e65eafc8f0361249b6ee19464d5b7044940b3690
Joshua Pereira <joshuathomaspereira@gmail.com>
2019-04-24 10:13:34 -0400

    create review model and add model validations

---
+++ app/models/review.rb
@@ -0,0 +1,45 @@
+class Review < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :body, length: { minimum: 20 }
+
+  validates :overall_rating, presence: true
+  validates :overall_rating, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5
+  }
+  validates :queue_time, presence: true
+  validates :queue_time, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5
+  }
+  validates :cleanliness, presence: true
+  validates :cleanliness, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5
+  }
+  validates :wifi, presence: true
+  validates :wifi, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5
+  }
+  validates :staff, presence: true
+  validates :staff, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5
+  }
+  validates :lounge_space, presence: true
+  validates :lounge_space, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5
+  }
+
+  belongs_to :airport
+
+end
