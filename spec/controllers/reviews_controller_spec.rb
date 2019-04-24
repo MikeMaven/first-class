e75cbd9d2792fca2b7c328c7025a84584eeb742d
Joshua Pereira <joshuathomaspereira@gmail.com>
2019-04-24 17:52:24 -0400

    Fix review faker and start controller test

---
+++ spec/controllers/reviews_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::ReviewsController, type: :controller do
+  describe 'GET#index' do
+    it 'returns successful response with json-formatted data' do
+      airport1 = FactoryBot.create(:airport)
+      review1 = FactoryBot.create(:review, airport: airport1)
+
+      get :index
+      returned_json = JSON.parse(response.body)
+
+      expect(response.status).to eq(200)
+      expect(response.content_type).to eq('application/json')
+
+      expect(returned_json.length).to eq(1)
+      expect(returned_json['airports'][0]['id']).to eq(airport1.id)
+      expect(returned_json['airports'][0]['name']).to eq(airport1.name)
+      expect(returned_json['airports'][0]['location']).to eq(airport1.location)
+      expect(returned_json['airports'][0]['airport_code']).to eq(airport1.airport_code)
+    end
+  end
+end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -17,13 +17,14 @@ FactoryBot.define do
 
   factory :review do
     title { Faker::Marketing.buzzwords }
-    body { Faker::Quote.famous_last_words }
+    body { (0...50).map { ('a'..'z').to_a[rand(26)] }.join }
     overall_rating { Faker::Number.within(1..5) }
     queue_time { Faker::Number.within(1..5) }
     cleanliness { Faker::Number.within(1..5) }
     wifi { Faker::Number.within(1..5) }
     staff { Faker::Number.within(1..5) }
     lounge_space { Faker::Number.within(1..5) }
+    airport { FactoryBot.create(:airport) }
   end
 
 end
