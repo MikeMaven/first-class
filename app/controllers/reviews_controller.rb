6bb385fdc8afb30d004120d493fc987e89144c69
Joshua Pereira <joshuathomaspereira@gmail.com>
2019-04-29 11:34:32 -0400

    Add authorization to view edit page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,15 @@
 class ReviewsController < ApplicationController
+  before_action :authorize_user
+
   def edit
     @review = Review.find(params[:id])
   end
   def update
+  end
+
+  def authorize_user
+    if !user_signed_in? || Review.find(params[:id]).user != current_user
+      raise ActionController::RoutingError.new("Not authorized to view this page. :[")
+    end
   end
 end
