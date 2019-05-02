1934b7034549207f17f2a29203c56c0e7cf84ebf
Joshua Pereira <joshuathomaspereira@gmail.com>
2019-05-02 15:23:21 -0400

    Implement feature and fix test

--- app/javascript/react/components/Airport.js
+++ app/javascript/react/components/Airport.js
@@ -5,6 +5,14 @@ const Airport = props => {
   return(
     <div className="airport-tile">
       <p><Link to={`/airports/${props.id}`}>({props.airport_code}) {props.name}</Link></p>
+      <ul>
+        <li> Average Overall Rating: {props.overall_rating} </li>
+        <li> Average Queue Time Rating: {props.queue_time_rating} </li>
+        <li> Average Cleanliness Rating: {props.cleanliness_rating} </li>
+        <li> Average Wifi Rating: {props.wifi_rating} </li>
+        <li> Average Staff Rating: {props.staff_rating} </li>
+        <li> Average Lounge Space Rating: {props.lounge_space_rating} </li>
+      </ul>
     </div>
   )
 }
